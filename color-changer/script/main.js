67859679ac9ce676f4f5b4de9c0719d9a0a0e99d
N4N0 <dantavious.w20@gmail.com>
2021-03-11 17:23:19 +0300

    updated color-changer

--- color-changer/script/main.js
+++ color-changer/script/main.js
@@ -2,5 +2,7 @@ root = document.documentElement;
 
 function changeBG() {
   let randomHex = Math.floor(Math.random()*16777215).toString(16);
+  let hexText = document.getElementById('text');
   root.style.setProperty('--default-bg-color', `#${randomHex}`);
+  hexText.textContent = `Current Hex: #${randomHex}`;
 }
